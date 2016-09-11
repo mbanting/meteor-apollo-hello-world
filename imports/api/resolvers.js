7edc6ec320bec88ab8f89480a8957e5ecede337e
mbanting <mbanting@yahoo.com>
2016-09-11 09:35:32 -0700

    Fixed the findAll sequenlize query

--- imports/api/resolvers.js
+++ imports/api/resolvers.js
@@ -1,11 +1,12 @@
 
 import { Post } from './connectors';
 
+// create the resolve functions for the available GraphQL queries
 export default resolvers = {
 
     Query: {
         posts(_, args){
-            return Post.find({});
+            return Post.findAll({});
         },
     }
 };
