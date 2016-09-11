26357dca204c98e51cbc54d10f3ca07dfa049c5c
mbanting <mbanting@yahoo.com>
2016-09-11 10:29:10 -0700

    Add comments

--- imports/api/connectors.js
+++ imports/api/connectors.js
@@ -1,10 +1,12 @@
 import Sequelize from 'sequelize';
 
+// create the connection
 const db = new Sequelize('apollodemo', "root", null, {
     host: 'localhost',
     dialect: 'mysql'
 });
 
+// define the model
 const PostModel = db.define('post', {
     content: { type: Sequelize.STRING },
     views: {type: Sequelize.INTEGER}
@@ -12,7 +14,9 @@ const PostModel = db.define('post', {
     timestamps: false
 });
 
+// create the table if it doesn't exist yet
 db.sync();
 
+// export Post
 const Post = db.models.post;
 export { Post };
