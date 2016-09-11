64774261e38a763a6d8ff1a21526473569f4c9e1
mbanting <mbanting@yahoo.com>
2016-09-11 10:33:30 -0700

    Create the main app mounting point

--- client/main.js
+++ client/main.js
@@ -1,22 +1,17 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import React from 'react';
 
-import './main.html';
+import ApolloClient from 'apollo-client';
+import { meteorClientConfig } from 'meteor/apollo';
+import { ApolloProvider } from 'react-apollo';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+import App from '/imports/ui/App';
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+const client = new ApolloClient(meteorClientConfig());
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  render(<ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('app'));
 });
