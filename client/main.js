fc6f517595ebac4de74ad5c75bdb146f39c9749b
mbanting <mbanting@yahoo.com>
2016-10-19 21:55:47 -0700

    Added optimistic UI

--- client/main.js
+++ client/main.js
@@ -2,13 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import React from 'react';
 
-import ApolloClient from 'apollo-client';
+import ApolloClient, { addTypename } from 'apollo-client';
 import { meteorClientConfig } from 'meteor/apollo';
 import { ApolloProvider } from 'react-apollo';
 
 import App from '/imports/ui/App';
 
-const client = new ApolloClient(meteorClientConfig());
+const client = new ApolloClient({
+  queryTransformer: addTypename,
+  dataIdFromObject: (result) => {
+    if (result.id && result.__typename) {
+      return result.__typename + result.id
+    }
+    return null
+  }
+});
 
 Meteor.startup(() => {
   render(<ApolloProvider client={client}>
--- imports/ui/messageFormContainer.js
+++ imports/ui/messageFormContainer.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { MessageForm } from './messageForm';
+import update from 'react-addons-update';
+import { Random } from 'meteor/random'
 
 const addPost = gql`
     mutation addPostMutation ($content: String!) {
       addPost(content: $content) {
         id,
-        content
+        content,
+        views
       }
     }
 `;
 
 export default MessageFormContainer = graphql(addPost, {
     props: ({ mutate }) => ({
-        submit: (content) => mutate({ variables: { content } }),
+        submit: (content) => mutate({
+            variables: { content },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                addPost: {
+                    __typename: 'Post',
+                    id: Math.floor((Random.fraction() * 100000) + 1),
+                    content: content,
+                    views: null
+                },
+            },
+            updateQueries: {
+                PostsForDisplay: (prev, { mutationResult }) => {
+                    const newPost = mutationResult.data.addPost;
+                    return update(prev, {
+                        posts: {
+                            $push: [newPost],
+                        },
+                    });
+                },
+            },
+        }),
     }),
 })(MessageForm);
